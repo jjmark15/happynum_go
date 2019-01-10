2be0eabf42c60a7e5216b65399d2ad5c93ff9f74
Josh Jones <ohblonddev@gmail.com>
2019-01-10 17:38:59 +0000

    ishappy

--- src/happy_num.go
+++ src/happy_num.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 func contains(s []int, e int) bool {
 	for _, a := range s {
@@ -11,9 +16,20 @@ func contains(s []int, e int) bool {
 	return false
 }
 
+func squareSum(n int) int {
+	strN := strconv.Itoa(n)
+	total := 0
+	stringDigits := strings.Split(strN, "")
+	for _, v := range stringDigits {
+		if intV, err := strconv.Atoi(v); err == nil {
+			total += int(math.Pow(float64(intV), 2))
+		}
+	}
+	return total
+}
+
 func isHappy(n int) bool {
 	unhappyMarkers := []int{89, 145, 42, 37, 58, 20, 4, 16}
-
 	ss := n
 
 	for {
@@ -22,7 +38,7 @@ func isHappy(n int) bool {
 		} else if contains(unhappyMarkers, ss) {
 			return false
 		} else {
-			ss := ss + 1
+			ss = squareSum(ss)
 		}
 	}
 }
